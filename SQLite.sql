ced6d3ddc6d9b672818992ce1be2ecb1d9baa991
pratikbhosale12 <145963522+pratikbhosale12@users.noreply.github.com>
2023-09-28 11:11:10 +0530

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,20 @@
+/*create table Worker(
+  Worker_ID int,
+  First_Name varchar(50),
+  Last_Name varchar(50),
+  Salary int,
+  Joining_Date datetime,
+  Department varchar(50)
+  );
+
+
+Insert into Worker values (001,'Monika','Arora',100000,'2014-02-20 09:00:00','HR') */
+
+Insert into Worker values (002,'Monika','Arora',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (003,'Niharika','Verma',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (004,'Vishal','Singhal',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (005,'Amitabh','Singh',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (006,'Vivek','Bhati',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (007,'Vipul','Diwan',100000,'2014-02-20 09:00:00','HR')
+Insert into Worker values (008,'','Arora',100000,'2014-02-20 09:00:00','HR')
+SELECT * From Worker
